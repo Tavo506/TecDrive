20976439fd21b8e116f152236c879f77989205a6
Gustavo Blanco <tavo01@estudiantec.cr>
2021-11-20 17:51:16 -0600

    Navegación entre carpetas

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Contenido } from 'src/app/models/Contenido.model';
+import { Archivo, Contenido } from 'src/app/models/Contenido.model';
 import { DriveService } from 'src/app/services/drive.service';
+
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-home',
@@ -11,7 +13,11 @@ import { DriveService } from 'src/app/services/drive.service';
 export class HomeComponent implements OnInit {
 
   datos : Contenido;
-  usuario : string;
+  usuario : string = "";
+
+  pathActual : Contenido;
+
+  path : string[] = [];
 
   constructor(private driveService : DriveService, private activatedRoute:ActivatedRoute) { }
 
@@ -22,6 +28,71 @@ export class HomeComponent implements OnInit {
     })
 
     this.datos = this.driveService.getDrive(this.usuario);
+    this.pathActual = this.clone(this.datos);    
+  }
+
+
+  clone(lista : Contenido) : Contenido{
+    var res : Contenido;
+    
+    res = {...lista}
+
+    return lista
+  }
+
+
+  open(archivo : Archivo){
+    Swal.fire({
+      title: `${archivo.nombre}.${archivo.extension}`
+    })
+  }
+
+
+  goToPath(path : string[], carpeta : string){
+    path.push(carpeta);
+    this.go(path);
+  }
+
+  go(path : string[]){
+    this.pathActual = this.clone(this.datos);
+    var lista;
+    path.forEach(e => {
+      lista = this.pathActual.contenido as Object;
+      lista = lista.filter(n => n.nombre == e )[0] as Contenido
+      this.pathActual = lista;
+           
+    })
+    
+  }
+
+
+  goBackTo(ruta : string){ 
+    var newPath : string[] = [];
+    var parar = false;
+    this.path.forEach(e => {
+      
+      
+      if (!parar) { 
+
+        newPath.push(e);
+
+        if (e == ruta) {
+          parar = true;
+          
+          console.log(newPath);
+          this.path = newPath;
+          
+          this.go(newPath);          
+        }
+      }
+    });
+
+  }
+
+
+  goToRoot(){
+    this.pathActual = this.clone(this.datos);
+    this.path = []
   }
 
 }
--- src/app/services/drive.service.ts
+++ src/app/services/drive.service.ts
@@ -2,104 +2,104 @@ import { Injectable } from '@angular/core';
 import { Contenido } from '../models/Contenido.model';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class DriveService {
 
-  constructor() { }
+    constructor() { }
 
 
 
-  dummy: any = {
-    "Usuario_1" : {
-        "nombre" : "root",
-        "tipo" : "carpeta",
-        "creacion" : "16/11/2021",
-        "modificacion" : "16/11/2021",
-        "tamaño" : 5000,
-        "compartido" : [
-            {
-                "nombre" : "Usuario 2",
-                "tipo" : "carpeta",
-                "creacion" : "16/11/2021",
-                "modificacion" : "16/11/2021",
-                "tamaño" : 5000,
-                "contenido" : [
-                    {
-                        "nombre" : "Ejemplo 2",
-                        "tipo" : "archivo",
-                        "extension" : "txt",
-                        "creacion" : "16/11/2021",
-                        "modificacion" : "16/11/2021",
-                        "tamaño" : 5000,
-                        "contenido" : "Hola Mundo 2!!",
-                        "compartido" : []
-                    }
-                ]
-            }
-        ],
-        "contenido" : [
-            {
-                "nombre" : "Compartidos",
-                "tipo" : "carpeta",
-                "creacion" : "16/11/2021",
-                "modificacion" : "16/11/2021",
-                "tamaño" : 5000,
-                "contenido" : []
-            },
-            {
-                "nombre" : "Ejemplo",
-                "tipo" : "archivo",
-                "extension" : "txt",
-                "creacion" : "16/11/2021",
-                "modificacion" : "16/11/2021",
-                "tamaño" : 5000,
-                "contenido" : "Hola Mundo!!",
-                "compartido" : []
-            }
-        ]
-    },
+    dummy: any = {
+        "Usuario_1": {
+            "nombre": "root",
+            "tipo": "carpeta",
+            "creacion": "16/11/2021",
+            "modificacion": "16/11/2021",
+            "tamaño": 5000,
+            "compartido": [],
+            "contenido": [
+                {
+                    "nombre": "Compartidos",
+                    "tipo": "carpeta",
+                    "creacion": "16/11/2021",
+                    "modificacion": "16/11/2021",
+                    "tamaño": 5000,
+                    "contenido": [
+                        {
+                            "nombre": "Usuario 2",
+                            "tipo": "carpeta",
+                            "creacion": "16/11/2021",
+                            "modificacion": "16/11/2021",
+                            "tamaño": 5000,
+                            "contenido": [
+                                {
+                                    "nombre": "Ejemplo 2",
+                                    "tipo": "archivo",
+                                    "extension": "txt",
+                                    "creacion": "16/11/2021",
+                                    "modificacion": "16/11/2021",
+                                    "tamaño": 5000,
+                                    "contenido": "Hola Mundo 2!!",
+                                    "compartido": []
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    "nombre": "Ejemplo",
+                    "tipo": "archivo",
+                    "extension": "txt",
+                    "creacion": "16/11/2021",
+                    "modificacion": "16/11/2021",
+                    "tamaño": 5000,
+                    "contenido": "Hola Mundo!!",
+                    "compartido": []
+                }
+            ]
+        },
 
-    "Usuario_2" : {
-        "nombre" : "root",
-        "tipo" : "carpeta",
-        "creacion" : "16/11/2021",
-        "modificacion" : "16/11/2021",
-        "tamaño" : 5000,
-        "compartido" : [],
-        "contenido" : [
-            {
-                "nombre" : "Compartidos",
-                "tipo" : "carpeta",
-                "creacion" : "16/11/2021",
-                "modificacion" : "16/11/2021",
-                "tamaño" : 5000,
-                "contenido" : []
-            },
-            {
-                "nombre" : "Ejemplo 2",
-                "tipo" : "archivo",
-                "extension" : "txt",
-                "creacion" : "16/11/2021",
-                "modificacion" : "16/11/2021",
-                "tamaño" : 5000,
-                "contenido" : "Hola Mundo 2!!",
-                "compartido" : [
-                    "Usuario 1"
-                ]
-            }
-        ]
+        "Usuario_2": {
+            "nombre": "root",
+            "tipo": "carpeta",
+            "creacion": "16/11/2021",
+            "modificacion": "16/11/2021",
+            "tamaño": 5000,
+            "compartido": [],
+            "contenido": [
+                {
+                    "nombre": "Compartidos",
+                    "tipo": "carpeta",
+                    "creacion": "16/11/2021",
+                    "modificacion": "16/11/2021",
+                    "tamaño": 5000,
+                    "contenido": []
+                },
+                {
+                    "nombre": "Ejemplo 2",
+                    "tipo": "archivo",
+                    "extension": "txt",
+                    "creacion": "16/11/2021",
+                    "modificacion": "16/11/2021",
+                    "tamaño": 5000,
+                    "contenido": "Hola Mundo 2!!",
+                    "compartido": [
+                        "Usuario 1"
+                    ]
+                }
+            ]
+        }
     }
-}
 
 
 
 
 
 
-getDrive(userName: string) : Contenido {
+    getDrive(userName: string): Contenido {
 
-  return this.dummy[userName] as Contenido;
-}
+        return this.dummy[userName] as Contenido;
+    }
 
 }
